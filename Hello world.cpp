792d08ef30c7400ca5575c3a23623a421de58ec4
Shreya B Yadav <149358632+Shreyayadav05@users.noreply.github.com>
2026-01-12 21:12:05 +0530

    Add Hello World program in C++

---
+++ Hello world.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl; // Output: Hello World!
+    return 0;
+}
+
+
+
+\* cout: This is used to print output to the console.
+
+<<: The insertion operator is used to send data to the output stream.
+
+endl: Inserts a newline after the output.
+*/
