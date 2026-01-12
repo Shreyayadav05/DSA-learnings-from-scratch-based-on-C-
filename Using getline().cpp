e6f963602d4d97e96939d19c91298ae183b7993f
Shreya B Yadav <149358632+Shreyayadav05@users.noreply.github.com>
2026-01-12 21:16:47 +0530

    Create Using getline().cpp

---
+++ Using getline().cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s); // Input: Hello World!
+    cout << s; // Output: Hello World!
+    return 0;
+}
+
+
+
+/*To read an entire sentence (including spaces), use getline().*/
