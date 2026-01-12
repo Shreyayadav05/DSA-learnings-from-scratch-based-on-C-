7ba50381df056ad26edcc3e65b42d01392048560
Shreya B Yadav <149358632+Shreyayadav05@users.noreply.github.com>
2026-01-12 21:34:32 +0530

    Create if-else statement.cpp

---
+++ if-else statement.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int runs;
+    cin >> runs; // Input: 90
+    if (runs >= 100) {
+        cout << "Century";
+    } 
+    else {
+        cout << "Not a Century"; // Output: Not a Century
+    }
+    return 0;
+}
