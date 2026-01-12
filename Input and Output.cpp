268ef238929abdcce4847b9956d7ae60f99eebde
Shreya B Yadav <149358632+Shreyayadav05@users.noreply.github.com>
2026-01-12 21:14:17 +0530

    Add Input and Output example in C++
    Implement basic input and output functionality.

---
+++ Input and Output.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a; // Input: 5
+    cout << "Output: " << a * 5 << endl; // Output: 25
+    return 0;
+}
+/*cin: Used to take input from the user.
+
+>>: The extraction operator is used to read data from the input stream.
+*/
