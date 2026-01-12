5cfe38ddd9cf18bd6407bf8c27913ab80a72a1b3
Shreya B Yadav <149358632+Shreyayadav05@users.noreply.github.com>
2026-01-12 21:17:15 +0530

    Add program to calculate the sum of two numbers

---
+++ addition of two numbers.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b; // Input: 2 5
+    cout << "Sum: " << a + b << endl; // Output: Sum: 7
+    return 0;
+}
