34b29bb58fcb38d9b3cbdf2e03061c450060211e
Shreya B Yadav <149358632+Shreyayadav05@users.noreply.github.com>
2026-01-21 23:04:11 +0530

    Create Arithmatic operations on two numbers.cpp

---
+++ Arithmatic operations on two numbers.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long a;
+    int b;
+    cin>>a>>b;
+    cout<<fixed<<setprecision(2);
+    cout<<(double)a/b<<endl;
+
+    return 0;
+}
